6125b3331ac1d5abaa0878d3f80b4d3cb00d0fa3
Eric <ye_jin2004@163.com>
2025-07-03 00:04:47 +0800

    新的风暴已经出现

--- agent.py
+++ agent.py
@@ -322,10 +322,10 @@ def plan_main(request: dict, task_id: int, debug_mode=False):
 if __name__ == "__main__":
     request_data = {
         "startCity": "上海",
-        "destinationCity": "南京",
+        "destinationCity": "北京",
         "peopleCount": 2,
         "daysCount": 3,
-        "additionalRequirements": "我想要进行一次红色之旅, 多去几个红色景点",
+        "additionalRequirements": "我想要进行一次红色之旅, 多去几个红色景点. 我也想吃火锅和北京烤鸭",
     }
     print(plan_main(request=request_data, task_id=1, debug_mode=True))
     # print(modify_plan(modify_str="也要吃烧烤", task_id=0, debug_mode=True))
